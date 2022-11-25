9948a97c6861f5b3d28eb1b462621c4be3476d48
ch543ch543 <ch543ch543@gmial.com>
2022-11-25 17:14:03 +0000

    fix bug in shipping page

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import CheckoutWizard from "../components/checkout/CheckoutWizard";
 import { navAnimation } from "../lib/animation";
 
